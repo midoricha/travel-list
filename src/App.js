648f2f4a3a4be2fd7ed87f75538995cc96c03e03
leah <leahw7@protonmail.com>
2024-10-07 12:56:14 -0400

    update DOM when editing forms; save form object

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -18,15 +20,28 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault(); // prevent default page reload after submitting
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))} // Cast from string to number
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1)         // create an array from 1 - 20 using JS
           .map((num) => (
             <option value={num} key={num}>
@@ -34,7 +49,11 @@ function Form() {
             </option>
           ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
